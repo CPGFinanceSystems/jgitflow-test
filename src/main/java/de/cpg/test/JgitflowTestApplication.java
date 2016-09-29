d3e1358a18d758538929d37767bc646c5e6739c4
Forstner Michael <michael.forstner@cpg.de>
2016-09-29 16:07:11 +0200

    Add feature (command line runner)

--- src/main/java/de/cpg/test/JgitflowTestApplication.java
+++ src/main/java/de/cpg/test/JgitflowTestApplication.java
@@ -1,12 +1,17 @@
 package de.cpg.test;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JgitflowTestApplication {
+public class JgitflowTestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JgitflowTestApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... strings) throws Exception {
 	}
 }
