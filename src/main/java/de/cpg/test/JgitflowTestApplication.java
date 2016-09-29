9a41db39acd57d66616496b8caae53e7e45a78ba
Forstner Michael <michael.forstner@cpg.de>
2016-09-29 16:10:56 +0200

    Fix for release of first version (add lombok)

--- src/main/java/de/cpg/test/JgitflowTestApplication.java
+++ src/main/java/de/cpg/test/JgitflowTestApplication.java
@@ -1,9 +1,11 @@
 package de.cpg.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class JgitflowTestApplication implements CommandLineRunner {
 
@@ -13,5 +15,6 @@ public class JgitflowTestApplication implements CommandLineRunner {
 
 	@Override
 	public void run(final String... strings) throws Exception {
+        log.info("First release");
 	}
 }
